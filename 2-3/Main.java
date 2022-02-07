d68745206ced4d27138067bb085c6ef7b6907e5a
KENZO TSUJI <58035269+tsujike@users.noreply.github.com>
2022-02-07 21:22:59 +0900

    Create Main.java

---
+++ 2-3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Mammal hippo = new Mammal();
+        Bird sparrow = new Bird();
+
+        hippo.cry();
+        sparrow.move();
+    }
+}
