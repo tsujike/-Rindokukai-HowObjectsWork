a844ac0754fe6cd93612b0b0c68def738a4be50d
KENZO TSUJI <58035269+tsujike@users.noreply.github.com>
2022-02-07 21:19:48 +0900

    Create Dog_Trainer.java

---
+++ 2-1/Dog_Trainer.java
@@ -0,0 +1,9 @@
+public class Dog_Trainer {
+    public static void main(String[] args) {
+        Dog pochi = new Dog("pochi");
+        Dog taro = new Dog("taro");
+        
+        System.out.println(pochi.cry());
+        System.out.println(taro.cry());
+    }
+}
